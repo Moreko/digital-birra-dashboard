1bb383098dfe47e6c64a3e981922d57cc68d7d58
rodtob <rodrigo.toblli@gmail.com>
2021-01-21 13:45:38 -0300

    prueba smallcard valores

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -5,7 +5,7 @@ import SmallCard from './SmallCard';
 let productInDataBase = {
     color:   "success",
     titulo: "Productos en DB",
-    valor: 550,
+    valor: 0,
     icono: "fa-clipboard-list",
     moneda: false 
 }
@@ -13,7 +13,7 @@ let productInDataBase = {
 let amount ={
     color:   "success",
     titulo: "Valor total",
-    valor: 546,
+    valor: 0,
     icono: "fa-dollar-sign",
     moneda: true
 }
@@ -21,7 +21,7 @@ let amount ={
 let user = {
     color:   "success",
     titulo: "Cantidad de usuarios",
-    valor: 38,
+    valor: 0,
     icono: "fa-user-check",
     moneda: false
 }
@@ -34,7 +34,9 @@ class ContentRowTop extends Component{
     constructor(props){
         super(props)
         this.state={
-            productos:[]
+            productos:[],
+            usuarios:0,
+            total:0
         }
     }
 
@@ -47,6 +49,15 @@ class ContentRowTop extends Component{
             this.setState({productos:productos.data})
         }) 
         .catch(error => console.log(error))
+
+        fetch("/api/users")
+        .then(respuesta=>{
+            return respuesta.json()
+        })
+        .then(usuarios => {
+            this.setState({usuarios:usuarios.meta.total})
+        }) 
+        .catch(error => console.log(error))
     }
 
     render() { 
@@ -54,11 +65,14 @@ class ContentRowTop extends Component{
         <React.Fragment>
         {/*<!-- Content Row -->*/}
         <div className="row">
-            {
+            {/* {
                 cardProps.map((producto,index)=>{
-                    return <SmallCard  {...producto}  key= {index}/>
+                    return <SmallCard  {...producto} valor={this.state.usuarios} key= {index}/>
                 })
-            }      
+            }       */}
+            <SmallCard  {...productInDataBase} valor={this.state.productos.length -1} key= {0}/>
+            <SmallCard  {...amount} valor={0} key= {1}/>
+            <SmallCard  {...user} valor={this.state.usuarios} key= {2}/>
         </div>
         </React.Fragment>
     )
