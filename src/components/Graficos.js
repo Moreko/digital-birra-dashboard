994e0f4c66064536b7f1ba69f0ad3f6aec214bf9
rodtob <rodrigo.toblli@gmail.com>
2021-01-27 23:30:38 -0300

    agrego graficos

--- src/components/Graficos.js
+++ src/components/Graficos.js
@@ -8,25 +8,15 @@ class Graficos extends Component{
         super(props)
         this.state={
             chartData:{
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['birras...'],
         datasets: [{
             label: 'cargando birras...',
-            data: [],
+            data: [10],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'
             ],
             borderWidth: 1
         }]
@@ -78,13 +68,24 @@ class Graficos extends Component{
             <div className="chart">
                <Bar
                     data={this.state.chartData}
-                    width={1000}
+                    width={750}
                     height={50}
                     options={{
                        maintainAspectRatio:false
                     }} 
                />
             </div>
+            <div className="chart">
+               <Pie
+                    data={this.state.chartData}
+                    width={750}
+                    height={50}
+                    options={{
+                       maintainAspectRatio:false
+                    }} 
+               />
+            </div>
+
 
         </React.Fragment>
     )}
